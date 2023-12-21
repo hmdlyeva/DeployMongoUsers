e6312ba5503fc6eeae72ef474be21ab773af4998
unknown <bd7fcqzu3@code.edu.az>
2023-12-22 01:42:31 +0400

    cors

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 require("dotenv").config();
 require("./src/config/db");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+const cors = require("cors");
+app.use(cors());
 const port = process.env.PORT || 3000;
 const routes = require("./src/routers/routers");
 app.use("/", routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
